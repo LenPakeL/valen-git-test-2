29fa540c0a680a893d394b045d66e4b572f57678
Valen <sugoi.desu77@gmail.com>
2022-05-19 19:36:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,8 @@
 <body>
     <h1>hellpo world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusamus alias voluptas impedit reiciendis cumque est, delectus totam minima facere porro eius maiores saepe excepturi aspernatur doloribus perspiciatis, odio nam.</p>
+    <ul>
+        <li>satu</li>
+    </ul>
 </body>
 </html>
